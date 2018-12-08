6c0504ee979b5f5dafa8e2b074a4378df2b7e793
root <root@localhost.localdomain>
2018-12-08 12:08:06 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+
+docker build -t marlovil/multi-client:latest -t marlovil/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t marlovil/multi-server:latest -t marlovil/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t marlovil/multi-worker:latest -t marlovil/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push marlovil/multi-client:latest
+docker push marlovil/multi-server:latest
+docker push marlovil/multi-worker:latest
+docker push marlovil/multi-client:$SHA
+docker push marlovil/multi-server:$SHA
+docker push marlovil/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=marlovil/multi-server$SHA
+kubectl set image deployments/client-deployments client=marlovil/multi-client$SHA
+kubectl set image deployments/worker-deployments worker=marlovil/multi-worker$SHA
+
+
+
